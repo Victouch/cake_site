a069c06c7de035f4fc81a697dbb2b4059c2fad90
Victouch <vicodion11@gmail.com>
2023-02-17 07:30:45 +0100

    --ALL

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):     
+    product_name = models.CharField(max_length=500)
+    product_image = models.ImageField(upload_to="images")
+    product_price = models.IntegerField()
