5cbcbd79dd9638c6ea64b1b90288c1bf708612f5
Victouch <vicodion11@gmail.com>
2023-02-24 11:23:24 +0100

    menu

--- cakeshop/settings.py
+++ cakeshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'about',
+    'menu',
 ]
 
 MIDDLEWARE = [
--- cakeshop/urls.py
+++ cakeshop/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace="home")),
-    path('', include('about.urls', namespace="about"))
+    path('', include('about.urls', namespace="about")),
+    path('', include('menu.urls', namespace="menu"))
 ]
 if settings.DEBUG:         
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
